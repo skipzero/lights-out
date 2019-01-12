56cd8ada0d71a23402854f1044b54845969af5b4
Jinx <jinx@Hold-Fast.local>
2019-01-11 19:45:00 -0800

    puhing change to switch machines

--- lights-out/src/Board.js
+++ lights-out/src/Board.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import light from './components/light.js';
+// import light from './components/light.js';
 
 class Board extends Component {
 
-  clickHandler() {
-    console.log(this.state)
-    if(this.on === false) {
-      return;
-    }
-    if(this.on === true) {
+  clickHandler(props) {
+    console.log('brain', this)
+    // if(this.off === false) {
+    //   return;
+    // }
+    // if(this.on === true) {
       const myLight = this.id;
       let coLights = [myLight - 1, myLight + 1, myLight - 5, myLight + 5 ]
       console.log(coLights, myLight);
@@ -18,26 +18,31 @@ class Board extends Component {
           return null;
         }
         acc[index] = {id: curr - 1, on: true}
-        console.log(acc)
+        console.log('acc',acc)
         return acc;
 
       }, []);
-      console.log(newState, this);
+      console.log('newstate',newState, this);
+      this.setState(newState);
       return newState;
-    }
+    // }
   }
 
   render() {
     console.log('light', this.props);
     const props = this.props;
+
     return (
       <div className='game'>
         <h3>Lights Out Game</h3>
         <div className='board'>
         { props.lights.map((myLight, indx) => {
           let {id, on} = myLight;
-          console.log(id, on)
-          return light(id, on);
+          console.log(id, on);
+          return (<div key={id} id={id} className='light' onClick={this.clickHandler.bind(this)}>
+            <span key={id} className={on ? 'on' : 'off'}>{id}</span>
+          </div>)
+          // return light(id, on);
         })}
         </div>
       </div>
--- lights-out/src/components/light.js
+++ lights-out/src/components/light.js
@@ -3,7 +3,7 @@ import React from 'react';
 const light = ({id, on}) => {
   return (
     <div key={id} id={id} className='light'>
-      <span className={on ? 'on' : 'off'}>{id}</span>
+      <span key={id} className={on ? 'on' : 'off'}>{id}</span>
     </div>
   )
 }
